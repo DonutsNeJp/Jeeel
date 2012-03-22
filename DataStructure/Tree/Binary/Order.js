272c912e17b6577be78d728c9cb8e71ede21d824
shimada <shimada@donuts.ne.jp>
2012-03-22 19:32:28 +0900

    merge

--- DataStructure/Tree/Binary/Order.js
+++
@@ -1,7 +0,0 @@
-
-/**
- * 二分木の内部の並び順の列挙体
- */
-Jeeel.DataStructure.Tree.Binary.Order = {
-    LEFT_LESS: 0
-};
--- Number/GetAvg.js
+++
@@ -1,8 +0,0 @@
-/**
- * 平均値を求める
- * 
- * @param {Number} var_args 平均値を求める際に数を可変で引き渡す
- */
-Jeeel.Number.getAvg = function (var_args) {
-    return this.getSum.apply(this, arguments) / arguments.length;
-};
